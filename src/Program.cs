3a34ba4a6ef8d86a4fafb2f35374974d9e980b62
Branden Barber <bbarber@Brandens-Laptop.local>
2024-10-13 09:44:18 -0500

    Switch to Emgu and display image from webcam

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+
+var win1 = "Test Window (Press any key to close)"; //The name of the window
+CvInvoke.NamedWindow(win1); //Create the window using the specific name
+using (Mat frame = new Mat(256, 256, DepthType.Cv8U, 3)) //Create a matrix to store the image)
+using (VideoCapture capture = new VideoCapture())
+    while (CvInvoke.WaitKey(1) == -1)
+    {
+        capture.Read(frame);
+        CvInvoke.Imshow(win1, frame);
+    }
