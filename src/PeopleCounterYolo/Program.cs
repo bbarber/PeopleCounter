f24422dee9c339b5327a38d60e0f4e3c0f8b9dd6
Branden Barber <bbarber@Brandens-Laptop.local>
2024-10-13 15:59:03 -0500

    More cleanup

--- src/PeopleCounterYolo/Program.cs
+++ src/PeopleCounterYolo/Program.cs
@@ -3,11 +3,16 @@ using Yolov8Net;
 using Emgu.CV.CvEnum;
 using SixLabors.ImageSharp;
 using System.Diagnostics;
+using SixLabors.ImageSharp.PixelFormats;
+using Emgu.CV.Structure;
 
-using var yolo = YoloV8Predictor.Create("./Models/yolo11n.onnx");
-var filename = "./Assets/webcam.jpg";
-using var capture = new VideoCapture(0, VideoCapture.API.Any);
 var targetFps = 2;
+var previewWebcam = true;
+var previewWebcamImage = "./Assets/webcam.jpg";
+var modelPath = "./Models/yolo11n.onnx";
+
+using var yolo = YoloV8Predictor.Create(modelPath);
+using var capture = new VideoCapture(0, VideoCapture.API.Any);
 
 while (true)
 {
@@ -16,10 +21,13 @@ while (true)
     // Take an image
     capture.Set(CapProp.AutoExposure, 20);
     var webcamImage = capture.QueryFrame();
-    webcamImage.Save(filename);
+    var imageBytes = webcamImage.ToImage<Bgr, byte>().ToJpegData();
+
+    // Optionally save the image to disk (for debugging)
+    if (previewWebcam) webcamImage.Save(previewWebcamImage);
 
     // Load the image and predict
-    using var image = Image.Load("./Assets/webcam.jpg");
+    using var image = Image.Load<Rgba32>(imageBytes); //Image.Load("./Assets/webcam.jpg");
     var predictions = yolo.Predict(image);
 
     Console.WriteLine("Predictions:");
