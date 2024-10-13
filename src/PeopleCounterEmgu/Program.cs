5af5e36b3a2419fa7aa198f32a9e72a31789ef8c
Branden Barber <bbarber@Brandens-Laptop.local>
2024-10-13 10:58:41 -0500

    Yolo project. Working predictions

---
+++ src/PeopleCounterEmgu/Program.cs
@@ -0,0 +1,22 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Models;
+
+var win1 = "Test Window (Press any key to close)"; //The name of the window
+CvInvoke.NamedWindow(win1); //Create the window using the specific name
+using (Mat frame = new(256, 256, DepthType.Cv8U, 3)) //Create a matrix to store the image)
+using (VideoCapture capture = new VideoCapture())
+    while (CvInvoke.WaitKey(1) == -1)
+    {
+        capture.Read(frame);
+        CvInvoke.Imshow(win1, frame);
+    }
+
+var imageIn = new Mat(256, 256, DepthType.Cv8U, 3);
+var imageOut = new Mat(256, 256, DepthType.Cv8U, 3);
+
+using (PedestrianDetector detector = new PedestrianDetector())
+{
+    await detector.Init();
+    var text = detector.ProcessAndRender(imageIn, imageOut);
+}
---
+++ src/PeopleCounterYolo/Program.cs
@@ -0,0 +1,27 @@
+﻿using Yolov8Net;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Processing;
+
+using var yolo = YoloV8Predictor.Create("./Models/yolov8m.onnx");
+
+// Provide an input image.  Image will be resized to model input if needed.
+using var image = SixLabors.ImageSharp.Image.Load("./Assets/doggo.jpg");
+var predictions = yolo.Predict(image);
+
+
+foreach (var pred in predictions)
+{
+    var originalImageHeight = image.Height;
+    var originalImageWidth = image.Width;
+
+    var x = Math.Max(pred.Rectangle.X, 0);
+    var y = Math.Max(pred.Rectangle.Y, 0);
+    var width = Math.Min(originalImageWidth - x, pred.Rectangle.Width);
+    var height = Math.Min(originalImageHeight - y, pred.Rectangle.Height);
+
+    // Bounding Box Text
+    string text = $"{pred.Label.Name} [{pred.Score}]";
+    Console.WriteLine(text);
+}
